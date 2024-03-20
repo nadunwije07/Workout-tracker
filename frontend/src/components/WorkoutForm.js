1269b1c138fb81ac55697a460247b6999dae2e2c
nadunwije07 <nadunwijeofficial@gmail.com>
2024-03-20 20:16:02 +0530

    Added the react context

--- frontend/src/components/WorkoutForm.js
+++ frontend/src/components/WorkoutForm.js
@@ -1,22 +1,19 @@
 import { useState } from 'react'
+import { useWorkoutsContext } from '../Hooks/useWorkoutsContext'
 
-
-// The WorkoutForm component is a form that allows the user to add a new workout to the database.
 const WorkoutForm = () => {
-  const [title, setTitle] = useState('') 
+  const { dispatch } = useWorkoutsContext()
+
+  const [title, setTitle] = useState('')
   const [load, setLoad] = useState('')
   const [reps, setReps] = useState('')
   const [error, setError] = useState(null)
 
-
-  // The handleSubmit function is called when the form is submitted. It sends a POST request to the /api/workouts endpoint with the workout data in the request body.
   const handleSubmit = async (e) => {
     e.preventDefault()
 
     const workout = {title, load, reps}
     
-
-    // The fetch function is used to send a POST request to the /api/workouts endpoint with the workout data in the request body. If the request is successful, the new workout is added to the database and the form is reset. If there is an error, the error message is displayed to the user.
     const response = await fetch('/api/workouts', {
       method: 'POST',
       body: JSON.stringify(workout),
@@ -24,7 +21,7 @@ const WorkoutForm = () => {
         'Content-Type': 'application/json'
       }
     })
-    const json = await response.json() // The response from the server is parsed as JSON.
+    const json = await response.json()
 
     if (!response.ok) {
       setError(json.error)
@@ -34,13 +31,11 @@ const WorkoutForm = () => {
       setTitle('')
       setLoad('')
       setReps('')
-      console.log('new workout added:', json)
+      dispatch({type: 'CREATE_WORKOUT', payload: json})
     }
 
   }
 
-
-  // The form has three input fields for the title, load, and number of reps of the workout. The values of these input fields are stored in the component's state using the useState hook. When the form is submitted, the handleSubmit function is called, which sends a POST request to the /api/workouts endpoint with the workout data in the request body.
   return (
     <form className="create" onSubmit={handleSubmit}> 
       <h3>Add a New Workout</h3>
